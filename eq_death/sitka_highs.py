67281ce2cb1a8640c8b82696fd870d312f279f37
szcbeihuang <1596956527@qq.com>
2023-06-02 20:06:06 +0800

    绘制天气数据的折线图

---
+++ eq_death/sitka_highs.py
@@ -0,0 +1,28 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+filename = 'sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs = [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        dates.append(current_date)
+        high = int(row[5])
+        highs.append(high)
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='blue')
+
+ax.set_title("2018", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("温度(F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
