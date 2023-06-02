461713ef8e64a5bed88c256665e7df415e751e77
szcbeihuang <1596956527@qq.com>
2023-06-02 21:22:30 +0800

    使用Plotly模拟投掷骰子和随机漫步

--- die_random/random_walk.py
+++ die_random/random_walk.py
@@ -2,7 +2,7 @@ from random import choice
 
 
 class RandomWalk:
-    def __init__(self, num_points=500):
+    def __init__(self, num_points=5000):
         self.num_points = num_points
 
         self.x_values = [0]
